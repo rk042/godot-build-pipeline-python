7742ee45f291eef3e98b55acde0a0f76299e4f54
rk042 <rathodketan042@gmail.com>
2025-12-26 15:20:49 +0530

    Added Window platform support and make it dynemic

--- build_pipline/build_pipeline.py
+++ build_pipline/build_pipeline.py
@@ -2,11 +2,6 @@ import json
 import subprocess
 import project_setup
 
-if not project_setup.godot_editor_path or not project_setup.your_project_path:
-    print("Godot editor path or project path is not set correctly.")
-    exit(1)
-
-script_path = "res://core_game/scripts/build_pipline_bridge.gd" #bridge script path in your project
 args=[{
     "version_code":"", # add manully version if you want, othewise leave it empty
     "spawn_rate":10
@@ -18,12 +13,11 @@ command = [
     project_setup.godot_editor_path,
     "--headless",
     "--path", project_setup.your_project_path,
-    "-s", script_path,
-    "--export-debug", "Android",
-    rf"{project_setup.build_output_path}\my_game.apk",
+    "-s", project_setup.script_path,
+    "--export-debug", project_setup.export_platform,
+    rf"{project_setup.build_output_path}\{project_setup.exported_project_name}",
     "--",payload
 ]
-
 
 print("Running build command:")
 print("Command:", command)
--- build_pipline/project_setup.py
+++ build_pipline/project_setup.py
@@ -4,7 +4,10 @@ try:
     godot_editor_path = r"E:\Godot_v4.5.1-stable_win64\Godot_v4.5.1-stable_win64_console.exe"
     your_project_path = r"D:\Projects\Godot\godot-build-pipeline-python"
     build_output_path = r""
-
+    exported_project_name = "build_pipline_test1"
+    export_platform = "android" # available options: "windows", "android"
+    script_path = "res://core_game/scripts/build_pipline_bridge.gd" # bridge script path in your project
+    
     if godot_editor_path is None or godot_editor_path == "":
         raise ValueError("Godot editor path is not set.")
     else:
@@ -20,5 +23,31 @@ try:
         build_output_path = str(pathlib.Path(your_project_path) / "builds/test")
         print(f"Build output path is not set. Using default: {build_output_path}")
 
+    if exported_project_name is None or exported_project_name == "":
+        raise ValueError("Exported project name is not set.")
+    else:
+        print(f"Exported project name is set to: {exported_project_name}")
+
+    if export_platform is None or export_platform == "":
+        raise ValueError("Export platform is not set.") 
+    else:
+        print(f"Export platform is set to: {export_platform}")
+
+    match export_platform.lower():
+        case "windows":
+            export_platform = "Windows"
+            exported_project_name += ".exe"
+        case "android":
+            export_platform = "Android"
+            exported_project_name += ".apk"
+        case _:
+           raise ValueError(f"Unsupported export platform: {export_platform}, please use 'windows' or 'android'.")
+
+    if script_path is None or script_path == "":
+        raise ValueError("Script path is not set.")
+    else:
+        print(f"Script path is set to: {script_path}")
+
 except Exception as e:
     print(f"Error: {e}")
+    exit(1)
